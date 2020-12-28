5657e30245c6a8833ceb989ae3227726dca26900
Hendra Wahyu <whysaputro@Hendras-MacBook-Pro.local>
2020-12-28 18:26:10 +0700

    store collective messages

---
+++ script.js
@@ -0,0 +1,14 @@
+let currentTime = new Date();
+currentTime = currentTime.toLocaleTimeString();
+
+let yourSuggestion = [];
+
+const snacks = ['French Fries','Donuts','Peanut Butter-Banana Pancakes','Almond-Maple Energy Bites','Fruit and Nut Coins'];
+const coffeeShops = ['Starbucks','Blue Bottle','Four Barrel Coffee','Colectivo Coffee','Tullys Coffee'];
+const sentences = [
+    "Humans are equal in God's eyes and everyone has the same rights. Like coffee that never chooses who can enjoy it.",
+    "When you wake up without a cup of coffee it makes you feel that one of the food chains is missing.",
+    "You and coffee are two things in common. They both make me fall in love.",
+    "As bitter as coffee, it will be even bitter if you are not there by my side.",
+    "Coffee teaches us that life is not always sweet, but also bitter."
+];
